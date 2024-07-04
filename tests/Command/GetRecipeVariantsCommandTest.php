82662c9138a63b6d5a11877fbf0c646ad4465a27
Viktorov <viktorovio@mail.ru>
2024-07-04 09:54:08 +0000

    fix env && add test

---
+++ tests/Command/GetRecipeVariantsCommandTest.php
@@ -0,0 +1,35 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Tests\Command;
+
+use Symfony\Bundle\FrameworkBundle\Console\Application;
+use Symfony\Bundle\FrameworkBundle\Test\KernelTestCase;
+use Symfony\Component\Console\Tester\CommandTester;
+
+class GetRecipeVariantsCommandTest extends KernelTestCase
+{
+    public function testExecute(): void
+    {
+        self::bootKernel();
+        $application = new Application(self::$kernel);
+
+        $command = $application->find('get:recipe:variants');
+        $commandTester = new CommandTester($command);
+        $commandTester->execute([
+            // pass arguments to the helper
+            'recipe' => 'd',
+
+            // prefix the key with two dashes when passing options,
+            // e.g: '--some-option' => 'option_value',
+            // use brackets for testing array value,
+            // e.g: '--some-option' => ['option_value'],
+        ]);
+
+        $commandTester->assertCommandIsSuccessful();
+
+        // the output of the command in the console
+        $output = $commandTester->getDisplay();
+    }
+}
